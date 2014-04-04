9174209d5d834314d09980fc34c5802b7b2a0199
Lovok <Lovok@users.noreply.github.com>
2014-04-04 15:12:15 +0200

    Add option for 3rd VTC network
    For future use ...

--- p2pool/networks.py
+++ p2pool/networks.py
@@ -86,7 +86,7 @@ nets = dict(
         VERSION_CHECK=lambda v: True,
     ),
 
-    vertcoin2=math.Object( # --> low hashrate net
+    vertcoin2=math.Object( # --> middle hashrate net
         PARENT=networks.nets['vertcoin'],
         SHARE_PERIOD=15, # seconds
         CHAIN_LENGTH=24*60*60//10, # shares
@@ -101,6 +101,25 @@ nets = dict(
         PERSIST=False,
         WORKER_PORT=9172,
         BOOTSTRAP_ADDRS='lovok.no-ip.com:9346 lovok.no-ip.com us-east.p2pools.net us-central.p2pools.net us-west.p2pools.net vert.marcsi.ch'.split(' '),
+        ANNOUNCE_CHANNEL='#p2pool-vtc',
+        VERSION_CHECK=lambda v: True,
+    ),
+    
+    vertcoin3=math.Object( # --> low hashrate net
+        PARENT=networks.nets['vertcoin'],
+        SHARE_PERIOD=15, # seconds
+        CHAIN_LENGTH=24*60*60//10, # shares
+        REAL_CHAIN_LENGTH=24*60*60//10, # shares
+        TARGET_LOOKBEHIND=200, # shares
+        SPREAD=12, # blocks
+        IDENTIFIER='a06a81c827cab975'.decode('hex'),
+        PREFIX='7c3614a6bcdcf796'.decode('hex'),
+        P2P_PORT=9348,
+        MIN_TARGET=4,
+        MAX_TARGET=2**256//2**20 - 1,
+        PERSIST=False,
+        WORKER_PORT=9173,
+        BOOTSTRAP_ADDRS='lovok.no-ip.com'.split(' '),
         ANNOUNCE_CHANNEL='#p2pool-vtc',
         VERSION_CHECK=lambda v: True,
     ),
